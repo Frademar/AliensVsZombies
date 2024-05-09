c88d9abdf26343991a470a02eaa8c14169aec287
Frademar <143592535+Frademar@users.noreply.github.com>
2024-05-09 15:20:40 +0200

    Add files via upload

---
+++ AliensVSZombies/Aliens.java
@@ -0,0 +1,30 @@
+public class Aliens extends Personaggio{
+    private static int countAliens=0;
+    public Aliens(int x, int y) {
+        super(x, y);
+        Aliens.countAliens++;
+    }
+    
+    public static int getCountAliens() {
+        return countAliens;
+    }
+    
+    public static void setCountAliens(int count){
+        Aliens.countAliens = count;
+    }
+
+    @Override
+    protected void die() {
+        System.out.println("Hai trovato uno zombies!! Laseeeratooooo!");
+    }
+
+    @Override
+    public void move(Campo campo) {
+        if(campo.isValidPosition(super.getX()+1, super.getY()+1)==false){
+            System.out.println("Posizione non valida sei arrivato al margine del banco.");
+        }
+        else{
+            campo.placePersonaggio(super.getX(), super.getY()+1, campo.whois(super.getX(), super.getY()));
+        }
+    }
+}
